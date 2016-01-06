9627448032f782d35e5c4867ff601647119fc98e
HappyLiuJian <liujianyousheng@gmail.com>
2016-01-06 19:35:07 +0800

    第一版展示自己

---
+++ fullJs/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by LiuJian on 2016/1/6.
+ */
+$(document).ready(function () {
+    $('.article').on('mouseover', function (e) {
+        e.stopPropagation();
+        $target = $(e.currentTarget);
+        var index = $target.index();
+        var length = $('.deck').children().length;
+        for(var i = 0; i <= index; i++) {
+            $('.deck').children().eq(i).css('top', 60 * i + 'px');
+        }
+        for(var j = index + 1; j < length; j++) {
+            $('.deck').children().eq(j).css('top', 60 * (j-1) + 280 + 'px');
+        }
+    })
+});
